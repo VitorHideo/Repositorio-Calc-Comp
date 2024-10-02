dfe36a971908644c87dc800d40aed55a2c189ccf
VitorHideo <vitor.frutuoso@sptech.school>
2024-10-02 14:25:54 -0300

    lista prova

---
+++ Lista-prova-AC2/ex3.r
@@ -0,0 +1,8 @@
+m6 = matrix(c(2,5,3,6), nrow = 2, ncol = 2, byrow = TRUE)
+m6
+
+m7 = matrix(c(5,8,3,2), nrow = 2, ncol = 2, byrow = TRUE)
+m7
+
+m8 = matrix(c(2,6,0,4,2,1), nrow = 2, ncol = 3, byrow = TRUE)
+m8
